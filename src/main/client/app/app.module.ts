889fbb67d5ed4b775d072c3e18c8e35cde8513c6
pwitiw <patryk.witiw@capgemini.com>
2016-12-08 16:41:56 +0100

    fixed module libraries

--- src/main/client/app/app.module.ts
+++ src/main/client/app/app.module.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/globals/angular/index.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 // import 'jquery';
 import * as angular from 'angular';
 import 'angular-ui-router';
