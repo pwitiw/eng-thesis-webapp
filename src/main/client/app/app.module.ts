b3de477fda72159b5cb2e9807599d82ff751a658
patryk <pwitiw93@gmail.com>
2016-12-15 22:27:24 +0100

    merged

--- src/main/client/app/app.module.ts
+++ src/main/client/app/app.module.ts
@@ -1,15 +1,13 @@
 /// <reference path="../typings/index.d.ts" />
 // import 'jquery';
-<<<<<<< HEAD
-import * as angular from 'angular';
+
+import * as angular from 'angular';
 import '../bower_components/angular-smart-table/dist/smart-table.min.js';
-import 'angular-ui-router';
-import 'templates';
-=======
+import 'angular-ui-router';
+import 'templates';
 import * as angular from 'angular';
 import 'angular-ui-router';
 import 'templates';
->>>>>>> 125fc2b9c3fe194aaab00190c051980b94ac3b29
 
 
 import ILocationProvider = angular.ILocationProvider;
@@ -22,13 +20,9 @@ import eventMgmtModuleName from './event-mgmt/event-mgmt.module'
 import mainPageModuleName from './main-page/main-page.module'
 
 
-angular.module('app', [mainPageModuleName, bookMgmtModuleName,orderMgmtModuleName, workerMgmtModuleName, eventMgmtModuleName, 'smart-table'])
-  .config(function ($urlRouterProvider:IUrlRouterProvider, $locationProvider:ILocationProvider) {
-    // $urlRouterProvider.when('', '/book-mgmt/book');
-=======
-angular.module('app', [mainPageModuleName, bookMgmtModuleName, orderMgmtModuleName, workerMgmtModuleName, eventMgmtModuleName])
+angular.module('app', [mainPageModuleName, bookMgmtModuleName, orderMgmtModuleName, workerMgmtModuleName, eventMgmtModuleName, 'smart-table'])
   .config(function ($urlRouterProvider: IUrlRouterProvider, $locationProvider: ILocationProvider) {
     $urlRouterProvider.when('', '/home');
->>>>>>> 125fc2b9c3fe194aaab00190c051980b94ac3b29
     $locationProvider.html5Mode(false);    //tutaj zeby mozna bylo z # cos todo dopytac o co kaman z tym
   });
+
